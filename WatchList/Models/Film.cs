6f74f09f8335f437e8a0dbc3ad4606588f7d5c61
Christina <yickind@hotmail.co.uk>
2023-05-24 00:02:07 +0100

    Added models for book, film and show

---
+++ WatchList/Models/Film.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatchList.Models
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
