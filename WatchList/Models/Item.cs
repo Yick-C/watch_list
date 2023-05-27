e7e168827cb1abd2294a5159e8b50d3606894f9c
Christina <yickind@hotmail.co.uk>
2023-05-27 23:22:55 +0100

    Combined models to only one Item file

---
+++ WatchList/Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WatchList.Models
+{
+    public enum Type
+    {
+        Book,
+        Film,
+        Show
+    }
+
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public Type SelectedItemType { get; set; }
+
+        [DisplayName("Created Date")]
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
